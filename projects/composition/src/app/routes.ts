2c913ddd9afbe021130767c7f06e1dfa6473d53e
kaplan81 <gesteira.pro@gmail.com>
2024-06-25 11:42:10 +0200

    use enum for route path

--- projects/composition/src/app/routes.ts
+++ projects/composition/src/app/routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { RoutedComponents } from './enums/routed.enum';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
+  {
+    path: RoutedComponents[RoutedComponents.contact],
+    component: ContactComponent,
+  },
 ];
