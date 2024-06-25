395bd056155f68f955eb0d0a246e6a79b70910ac
kaplan81 <gesteira.pro@gmail.com>
2024-06-25 09:30:16 +0200

    remove zones

--- projects/composition/src/app/app.config.ts
+++ projects/composition/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     // provideRouter(routes, withDebugTracing()),
     provideRouter(routes),
   ],
